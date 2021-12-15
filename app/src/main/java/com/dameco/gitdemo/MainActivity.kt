55ca1378b5127857d929322eae2b7114e0660087
davidmedinacortes <david.medina@soprasteria.com>
2021-12-15 10:51:06 +0100

    Second commit

--- app/src/main/java/com/dameco/gitdemo/MainActivity.kt
+++ app/src/main/java/com/dameco/gitdemo/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.dameco.gitdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("TAG", "Mensaje de prueba")
     }
 }
